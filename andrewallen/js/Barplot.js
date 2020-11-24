4143aff0d7afeb28a286cd3e4a9ec227aae090d9
Jared Wilber <j.wilber92@gmail.com>
2020-11-23 22:20:22 -0800

    adding barplot

--- andrewallen/js/Barplot.js
+++ andrewallen/js/Barplot.js
@@ -1,10 +1,11 @@
 // set the dimensions and margins of the graph
-const barMargin = { top: 20, right: 10, bottom: 50, left: 230 },
+const barMargin = { top: 10, right: 50, bottom: 50, left: 250 },
   barWidth = 800 - barMargin.left - barMargin.right,
-  barHeight = 800 - barMargin.top - barMargin.bottom;
+  barHeight = 850 - barMargin.top - barMargin.bottom;
+console.log('we in')
 
 // append the svg object to the body of the page
-let svg = d3.select("#my_dataviz")
+let svg = d3.select("#my_datavis")
   .append("svg")
   .attr("preserveAspectRatio", "xMinYMin meet")
   .attr("viewBox", `0 0 ${(barWidth + barMargin.left + barMargin.right)}
@@ -20,16 +21,17 @@ d3.csv("data/toptricks.csv", function (data) {
     .domain([0, d3.max(data, d => +d.count)])
     .range([0, barWidth]);
 
-  let xAxis = d3.axisBottom(x).tickSize(barHeight);
+  let xAxis = d3.axisBottom(x).tickSize(0);
   svg.append("g")
     .attr('class', 'x-axis')
-    .call(xAxis);
+    .attr('transform', `translate(0, ${barHeight})`)
+    .call(xAxis)
 
   // Y axis
   let y = d3.scaleBand()
     .range([0, barHeight])
     .domain(data.map(d => d.trick))
-    .padding(.14);
+    .padding(.46);
 
   svg.append("g")
     .attr('class', 'y-axis')
@@ -45,10 +47,8 @@ d3.csv("data/toptricks.csv", function (data) {
     .attr("y", d => y(d.trick))
     .attr("width", d => x(d.count))
     .attr("height", y.bandwidth())
-    .attr('rx', 6)
-    .attr('ry', 6)
-    .attr('fill', "tan")
-    .style('stroke', 'darkbrown')
+    .attr('fill', "black")
+    .style('stroke', 'black')
     .style('stroke-width', 7)
 
 })
@@ -58,7 +58,6 @@ d3.select('svg')
   .append('text')
   .attr('x', (barWidth + barMargin.right) / 2)
   .attr('y', barMargin.top / 1.1)
-  .html(`“Dude, no way would I do a pressure flip.”`)
   .style('font-family', "Bungee")
   .style('font-size', '1.2rem')
 
