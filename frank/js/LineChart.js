1ae91a44119249159c19be13b052bd05fda9af8e
halisewski <harrisonlisewski1@gmail.com>
2021-07-14 07:34:56 +1000

    added frank

---
+++ frank/js/LineChart.js
@@ -0,0 +1,213 @@
+class LineChart {
+	constructor(opts) {
+		this.data = opts.data;
+		this.element = opts.element;
+		this.metric = opts.metric;
+		this.dropdown = opts.dropdown;
+		this.xValue = opts.xValue;
+		this.yValue = opts.yValue;
+		this.title = opts.title;
+
+		const that = this;
+
+		this.MARGIN = { TOP: 10, BOTTOM: 70, LEFT: window.innerWidth / 9.5 + 5, RIGHT: window.innerWidth / 7 };
+		const width = window.innerWidth < 1000 ? (window.innerWidth / 1.05) : (window.innerWidth / 1.3)
+		this.WIDTH = width - this.MARGIN.RIGHT - this.MARGIN.LEFT;
+		this.HEIGHT = (window.innerHeight / 2.5) - this.MARGIN.TOP - this.MARGIN.BOTTOM;
+
+		this.svg = d3.select(this.element)
+			.append('svg')
+			.attr("preserveAspectRatio", "xMinYMin meet")
+			.attr('width', this.WIDTH + this.MARGIN.LEFT + this.MARGIN.RIGHT)
+			.attr('height', this.HEIGHT + this.MARGIN.TOP + this.MARGIN.BOTTOM)
+			.attr("viewBox", `0 0 ${this.WIDTH + this.MARGIN.LEFT + this.MARGIN.RIGHT} ${this.HEIGHT + this.MARGIN.TOP + this.MARGIN.BOTTOM}`)
+			.append('g')
+			.attr('transform', `translate(${this.MARGIN.LEFT}, ${this.MARGIN.TOP})`);
+
+		// add text labels
+		this.xLabel = this.svg.append('text')
+			.attr('x', this.WIDTH / 2)
+			.attr('y', this.HEIGHT + 50)
+			.attr('text-anchor', 'middle')
+			.text("Year")
+			.style('font-size', '1rem')
+
+
+		if (this.title) {
+			this.svg.append('text')
+				.attr('x', this.WIDTH / 2)
+				.attr('y', this.MARGIN.TOP - 2)
+				.attr('text-anchor', 'middle')
+				.style('text-decoration', 'underline')
+				.text(this.title)
+				.style('font-size', '1rem');
+		}
+
+
+		this.svg.append('text')
+			.attr('x', - this.HEIGHT / 2)
+			.attr('y', -30)
+			.attr('text-anchor', 'middle')
+			.text('Percentage')
+			.attr('transform', 'rotate(-90)')
+			.style('font-size', '1rem');
+
+
+		// preset axes
+		this.xAxisGroup = this.svg.append('g')
+			.attr('transform', `translate(0, ${this.HEIGHT})`);
+
+		this.yAxisGroup = this.svg.append('g');
+
+		this.tooltip = d3.select(this.element)
+			.append('div')
+			.attr('id', this.dropdown)
+			.attr('class', 'tooltip')
+			.style("opacity", 0)
+			.style("pointer-events", "none")
+			.style('font-size', '1rem')
+			.style('text-align', 'left')
+			.style('font-weight', 'bold')
+			;
+
+
+		d3.csv(this.data, data => {
+			this.data = data;
+			if (this.dropdown) {
+				this.metrics = [...new Set(data.map(d => d.obstacle))];
+				// dropdown to select metric
+				d3.select('#metric-select')
+					.selectAll('options')
+					.data(this.metrics)
+					.enter()
+					.append('option')
+					.html(d => d);
+			}
+
+			this.draw(this.metric);
+		})
+
+
+	}
+
+	draw(currMetric) {
+		const t = d3.transition().duration(1100).ease(d3.easeBack);
+		const that = this;
+		// this.title.text(`${currMetric} Counts`)
+		// set current data
+		let currData = this.data.filter(d => d.obstacle == currMetric);
+
+		const x = d3.scaleLinear()
+			.domain([d3.min(currData, d => d[this.xValue]) - 1,
+			d3.max(currData, d => d[this.xValue])])
+			.range([0, this.WIDTH]);
+
+		const y = d3.scaleLinear()
+			.domain([0,
+				d3.max(currData, d => +d[this.yValue]) + 3])
+			.range([this.HEIGHT, 0]);
+
+		this.lineActual = d3.line()
+			.x(function (d) { return x(d[that.xValue]) })
+			.y(function (d) { return y(+d[that.yValue]) })
+			.curve(d3.curveCatmullRom.alpha(.5))
+
+		const xAxisCall = d3.axisBottom(x).ticks(4).tickFormat(d3.format("d"));
+		this.xAxisGroup.transition().duration(500).call(xAxisCall)
+
+		const yAxisCall = d3.axisLeft(y);
+		this.yAxisGroup.transition().duration(500).call(yAxisCall)
+
+
+
+		// add line chart
+		let line = this.svg.selectAll('path.met')
+			.data([currData]);
+
+		let lineEnter = line.enter().append('path')
+			.attr('class', 'met');
+
+		line = line.merge(lineEnter);
+
+		line.transition(t)
+			.attrTween('d', function (d) {
+				var previous = d3.select(this).attr('d');
+				var current = that.lineActual(d);
+				return d3.interpolatePath(previous, current);
+			})
+			.style('stroke', 'black')
+			.style("stroke-width", window.innerWidth < 1000 ? 4 : 7)
+			.style("fill", "none")
+			.style('pointer-events', 'none')
+			.attr('stroke-linejoin', 'round')
+			.attr('active', true)
+			.style('opacity', .85)
+
+
+		// handle circles
+		// update circles
+		let circles = this.svg.selectAll(`circle.${this.yValue}`)
+			.data(currData, d => d[this.xValue]);
+
+		circles.exit().remove();
+
+		let circlesEnter = circles
+			.enter()
+			.append('circle')
+			.attr('class', 'cnt')
+			.attr('fill', 'black')
+			.style('stroke', 'rgb(248,209,6)')
+
+			.attr('r', 1)
+			.attr('cx', d => x(d[that.xValue]))
+			.attr('cy', d => y(+d[that.yValue]))
+
+
+
+		circles = circles.merge(circlesEnter);
+
+		circles.transition(t)
+			.attr('cx', d => x(d[that.xValue]))
+			.attr('cy', d => y(+d[that.yValue]))
+			.attr('r', window.innerWidth < 1000 ? 5 : 10)
+			.attr('stroke-width', 3)
+			.attr('stroke', 'white')
+
+		circles
+			.on("mouseover", function (d) { // show it and update html
+				let pointData = d;
+				d3.selectAll(`circle.${this.yValue}`)
+					.style('opacity', 0)
+				d3.selectAll(`circle.cnt`)
+					.style('opacity', .2)
+				d3.selectAll('path.met')
+					.style('opacity', .2)
+				d3.select(this)
+					.style('opacity', .7)
+				that.tooltip
+					.transition()
+					.style("opacity", .9);
+				that.tooltip.html(function (d) { 
+					let value = Math.round(pointData[that.yValue]) 
+					return `${value}%`
+				})
+					 .style("left", (d3.event.pageX + 10) + "px")
+					 .style("top", (d3.event.pageY - 10) + "px")
+					;
+			})
+			.on("mouseout", function (d) {
+				d3.selectAll(`circle.cnt`)
+					.style('opacity', 1)
+				d3.selectAll('path.met')
+					.style('opacity', 1)
+				that.tooltip.transition()
+					.style("opacity", 0)
+				;
+			})
+
+		
+
+
+	}
+
+}
---
+++ frank/js/Waffle.js
@@ -0,0 +1,176 @@
+let USER_VIDEO = "Cash_Money_Vagrant";
+
+
+function getValKey() {
+    return "grp" + USER_VIDEO;
+}
+
+let VAL_KEY = getValKey();
+console.log(USER_VIDEO)
+console.log(VAL_KEY)
+let grp_vals = {};
+
+let delay_per_unit = 30;
+let cell_color = "black";
+let bg_color = "rgb(248,209,6)";
+
+// Dimensions of single chart.
+let margin = { top: 0, right: 0, bottom: 0, left: 6 };
+let width = 134 - margin.left - margin.right;
+let height = 134 - margin.top - margin.bottom;
+
+
+
+
+d3.csv("data/square_pie.csv", function (error, data) {
+    if (error) throw error;
+    console.log(data)
+    let valfields = d3.keys(field_details);
+    // Make data accessible by grp key
+    data.forEach(function (o) {
+        grp_vals["grp" + o.video] = o;
+    });
+
+    //
+    // Setup grid.
+    //
+    let cells = [];
+    d3.select("#grid").text().split("\n").forEach(function (line, i) {
+        let re = /\w+/g, m;
+        while (m = re.exec(line)) cells.push({
+            name: m[0],
+            selected: 1,
+            x: m.index / 3,
+            y: i
+        });
+    });
+
+
+    //
+    // Make a square pie for each field.
+    //
+    valfields.forEach(function (v, i) {
+        let grid_width = d3.max(cells, d => d.x) + 2;
+        let grid_height = d3.max(cells, d => d.y) + 2;
+        let cell_size = width / grid_width;
+        let holder_width = width + margin.left + margin.right;
+
+
+        let chart_perc = grp_vals[VAL_KEY][v];
+
+
+        // create chartholder
+        let div = d3.select("#charts").append("div")
+            .attr("id", "holder" + v)
+            .attr("class", "chartholder")
+            .style("width", holder_width + "px");
+
+        // add h3 for each chart
+        div.append("h3")
+            .attr('class', 'h3title')
+            .append('text')
+            .text(`${field_details[v].desc}: ${chart_perc}%`);
+
+        // add svg
+        let svg = div.append("svg")
+            .attr("class", "squarepie")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        let cell = svg.append("g")
+            .attr("id", "vf" + v)
+            .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+            .selectAll(".cell")
+            .data(cells)
+            .enter().append("g")
+            .attr("class", "cell")
+            .attr("transform", d => "translate(" + (d.x - grid_width / 2) * cell_size + "," + (d.y - grid_height / 2) * cell_size + ")");
+
+        cell.append("rect")
+            .attr("x", -cell_size / 2)
+            .attr("y", -cell_size / 2)
+            .attr('width', cell_size)
+            .attr('height', cell_size)
+            .attr('rx', 3)
+            .attr('stroke', 'black')
+            .attr('stroke-width', .5)
+            .attr("fill", (d, i) => (i < (100 - grp_vals[VAL_KEY][v])) ? bg_color : cell_color)
+
+
+    }); // @end forEach()
+
+
+    d3.select("#charts").append("div").attr("class", "clr");
+
+
+
+    //
+    // Group selection with buttons
+    //
+    d3.selectAll("#percentages .button").on("click", function () {
+        USER_VIDEO = d3.select(this).attr("data-val");
+        d3.select("#percentages .current").classed("current", false);
+        d3.select(this).classed("current", true);
+        update();
+    });
+
+
+    // 
+    // Update based on current group
+    //
+    function update() {
+
+        let prev_val_key = VAL_KEY;
+        VAL_KEY = getValKey();
+
+        // Update charts.
+        valfields.forEach(function (v, k) {
+
+            let start_i = 100 - grp_vals[prev_val_key][v];
+            let end_i = 100 - grp_vals[VAL_KEY][v];
+            let new_perc = grp_vals[VAL_KEY][v];
+
+            // update title
+            d3.select(`#holder${v}`)
+                .select("h3")
+                .transition()
+                // .delay(800)
+                .text(`${field_details[v].desc}: ${new_perc}%`);
+
+            d3.select("#vf" + v).selectAll(".cell rect")
+                .transition()
+                .duration(750)
+                .delay(function (d, i) {
+
+                    // Decreasing
+                    if (start_i < end_i) {
+                        let curr_delay = (i - start_i) * delay_per_unit;
+                        curr_delay = Math.max(curr_delay, 0);
+                        return curr_delay;
+                    }
+
+                    // Increasing
+                    else if (start_i > end_i) {
+                        let curr_delay = (start_i - i) * delay_per_unit;
+                        curr_delay = Math.max(curr_delay, 0);
+                        return curr_delay;
+                    }
+
+                    // No change.
+                    else {
+                        return 0;
+                    }
+                })
+                .attr("fill", (d, i) => (i < (100 - grp_vals[VAL_KEY][v])) ? bg_color : cell_color)
+
+
+        }); // @end forEach()
+
+    } // @end update()
+
+
+
+
+}); // @end d3.tsv()
